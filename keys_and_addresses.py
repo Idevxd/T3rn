4f66b094fd6dbc03ec43b45f7b8f053fbde300a2
Idevxd <140291705+Idevxd@users.noreply.github.com>
2024-12-19 01:30:55 +0530

    Create keys_and_addresses.py

---
+++ keys_and_addresses.py
@@ -0,0 +1,11 @@
+#keys_and_addresses.py
+
+#Private key list (each private key should be a string)
+private_keys = [
+""
+]
+
+#Label list (each label corresponds to a private key)
+labels = [
+""
+]
